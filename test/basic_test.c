259401235399725254b4400878957d6a1c9eda26
Xinan Xu <xx2153@columbia.edu>
2013-12-13 00:20:26 -0500

    Init commit

---
+++ test/basic_test.c
@@ -0,0 +1,44 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "pthread.h"
+#define n 10000000
+pthread_t th[4];
+double a[4];
+void* run_0(void* arg){
+for(int i=0;i<n;++i)
+	a[0]++;
+	return NULL;
+}
+void* run_1(void* arg){
+for(int i=0;i<n;++i)
+	a[1]++;
+	return NULL;
+}
+void* run_2(void* arg){
+for(int i=0;i<n;++i)
+	a[2]++;
+	return NULL;
+}
+void* run_3(void* arg){
+for(int i=0;i<n;++i)
+	a[3]++;
+	return NULL;
+}
+int main(){
+
+	pthread_create(&th[0],NULL,run_0,NULL);
+	pthread_create(&th[1],NULL,run_1,NULL);
+	pthread_create(&th[2],NULL,run_2,NULL);
+	pthread_create(&th[3],NULL,run_3,NULL);
+for(int i=0;i<n;++i){
+	a[0]++;
+	a[1]++;
+	a[2]++;
+	a[3]++;
+}
+	pthread_join(th[0],NULL);
+	pthread_join(th[1],NULL);
+	pthread_join(th[2],NULL);
+	pthread_join(th[3],NULL);
+	return 0;
+}
